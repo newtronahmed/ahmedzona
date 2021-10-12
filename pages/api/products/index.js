690fabb3324a144b6cd861c7aaf70c3bbccd1df5
newtronahmed <bazubairu@st.ug.edu.gh>
2021-10-12 11:34:24 +0000

    using data.js as database for index page

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,9 +1,13 @@
 import nextConnect from "next-connect";
 import db from "../../../utils/db";
 import Product from "../../../models/product";
+import { data } from "../../../utils/data";
 const handler = nextConnect()
 handler.get(async function(req,res){
-    const products = await Product.find({})
-    res.send(products)
+    // db.connect2()
+    res.send(data.products)
+    // const products = await Product.find({})
+    // res.send(products)
 })
-export default db.connect(handler)
+// export default db.connect(handler)
+export default handler
