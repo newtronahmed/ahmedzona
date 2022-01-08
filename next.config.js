6458d86edc0881e074b3020d29671c54f1b6aa81
newtronahmed <bazubairu@st.ug.edu.gh>
2022-01-08 04:31:24 +0000

    added https:// to app url

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ module.exports = {
     "JWT_SECRET_KEY": "ahmedzona_jwt_key",
     "PAYPAL_CLIENT_ID":
       "AXc2KpgAIPqalxcV6rUW-Mb7unnGf6-epmWKf9z3hZagpPkY8phMefNFUFqsLPhJufXP-RN0E-qscNmr",
-    "APP_URL":"ahmedzona.vercel.app"
+    "APP_URL":"https://ahmedzona.vercel.app"
   },
 };
